eab13dd23337f746b374657d2b8a803bd9d1cd54
Roberto De las morenas <robertodelasmorenas2005@gmail.com>
2024-01-16 09:32:42 +0100

    Creaion de la propiedad nombre empleado

--- GestionNominasV1/Nomina.cs
+++ GestionNominasV1/Nomina.cs
@@ -15,5 +15,36 @@ namespace GestionNominasV1
       private string _puesto;
       private int _horasTrabajadas;
       private float _salarioHora;
+
+      // Propiedades 
+      /// <summary>
+      /// Nombre del empleado
+      /// </summary>
+      public string NombreEmpelado
+      {
+         get
+         {
+            // Cotrol de contendido
+            if (string.IsNullOrEmpty(_nombre))
+            {
+               throw new Exception("Nombre del trabajador no establecido");
+            }
+
+            // Devolucion del Valor
+            return _nombre;
+         }
+
+         set
+         {
+            // Validacion de lso datos a establecer
+            if (string.IsNullOrEmpty(value))
+            {
+               throw new Exception("El nombre del trabajador no puede ser null o la cadena vacia");
+            }
+
+            // Establecimiento del valor al miembro privado
+            _nombre = value;
+         }
+      }
    }
 }
