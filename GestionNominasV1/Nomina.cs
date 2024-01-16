d899e99cecb84fcc6ccad0c8009ae5fbd02bf32a
Roberto De las morenas <robertodelasmorenas2005@gmail.com>
2024-01-16 09:17:21 +0100

    Adicion de mienbros privados a la clase

--- GestionNominasV1/Nomina.cs
+++ GestionNominasV1/Nomina.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Runtime.InteropServices;
 using System.Text;
 using System.Threading.Tasks;
 
@@ -8,5 +9,11 @@ namespace GestionNominasV1
 {
    public class Nomina
    {
+      // Miembros pribados de la clase 
+      private string _nombre;
+      private string _apellido;
+      private string _puesto;
+      private int _horasTrabajadas;
+      private float _salarioHora;
    }
 }
